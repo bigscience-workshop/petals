f60a7dd183d618db2da0e6eaa5b7425d8ad6dc36
Dmitry Baranchuk <dbaranchuk@zomb-research-cv-5.zombie.yandex.net>
2022-06-29 13:52:43 +0300

    deploy swarm on local & remote machines

--- cli/deploy_server.sh
+++ cli/deploy_server.sh
@@ -5,12 +5,12 @@
 #################
 
 instructions() {
-  echo "Usage: $0 [-i] [ -d ] [ -s ] [ -b ] [-p] [-t]" >&2
+  echo "Usage: $0 [-i] [ -d ] [ -p ] [ -b ] [-a] [-t]" >&2
   echo " -i: initial peer"
   echo " -d: device" >&2
-  echo " -s: server_id" >&2
+  echo " -p: server identity path" >&2
   echo " -b: block_ids" >&2
-  echo " -p: port to run a server" >&2
+  echo " -a: host maddrs" >&2
   echo " -t: whether to run local tests" >&2
   exit 1
 }
@@ -19,18 +19,17 @@ if [ ! $# -ge 8 ]; then
     instructions
 fi
 
-while getopts ":i:d:s:b:p:t:" option; do
+while getopts ":i:d:p:b:a:t:" option; do
     case $option in
-        i) INITIAL_PEER=${OPTARG}
+        i)  INITIAL_PEER=${OPTARG}
             ;;
         d)  DEVICE=${OPTARG}
-            #((${DEVICE} == 'cpu' || ${DEVICE} == 'cuda')) || instructions
             ;;
-        s)  SERVER_ID=${OPTARG}
+        p)  SERVER_ID_PATH=${OPTARG}
             ;;
         b)  BLOCK_IDS=${OPTARG}
             ;;
-        p)  PORT=${OPTARG}
+        a)  HOST_MADDR=${OPTARG} # TODO: allow several maddrs 
             ;;
         t)  RUN_LOCAL_TESTS=true
             ;;
@@ -45,8 +44,8 @@ echo "= Config ="
 echo "=========="
 echo "Initial peer: ${INITIAL_PEER}"
 echo "Device: ${DEVICE}"
-echo "Server name: server${SERVER_ID}.id"
-echo "Server address: /ip4/127.0.0.1/tcp/${PORT}"
+echo "Server name: ${SERVER_ID_PATH}"
+echo "Server address: ${HOST_MADDR}"
 echo "Bloom blocks: ${BLOCK_IDS}"
 
 
@@ -54,6 +53,7 @@ echo "Bloom blocks: ${BLOCK_IDS}"
 # Install or activate env #
 ###########################
 
+# TODO fix bug with self calling
 source ~/miniconda3/etc/profile.d/conda.sh
 if conda env list | grep ".*bloom-demo.*"  >/dev/null 2>/dev/null; then
     conda activate bloom-demo
@@ -62,7 +62,6 @@ else
     conda activate bloom-demo
 
     conda install -y -c conda-forge cudatoolkit-dev==11.3.1 cudatoolkit==11.3.1 cudnn==8.2.1.32
-    # Specify -i https://pypi.org/simple at ultramar
     pip install -i https://pypi.org/simple torch==1.11.0+cu113 torchvision==0.12.0+cu113 -f https://download.pytorch.org/whl/torch_stable.html
     pip install -i https://pypi.org/simple accelerate==0.10.0 huggingface-hub==0.7.0 hivemind==1.1.0
     pip install -i https://pypi.org/simple bitsandbytes-cuda113==0.26.0
@@ -85,4 +84,4 @@ fi
 ##############
 
 python -m cli.run_server --prefix bloom6b3 --converted_model_name_or_path bigscience/test-bloomd-6b3 --device ${DEVICE} --initial_peer ${INITIAL_PEER} \
-  --block_indices ${BLOCK_IDS} --torch_dtype float32 --identity_path ./server${SERVER_ID}.id --host_maddrs /ip4/127.0.0.1/tcp/${PORT} 2> server_${SERVER_ID}.out
+  --block_indices ${BLOCK_IDS} --torch_dtype float32 --identity_path ${SERVER_ID_PATH} --host_maddrs ${HOST_MADDR} &> ${SERVER_ID_PATH}.log
