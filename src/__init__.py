b65409398a882330bde56e4ffa775d8db72607b0
Aleksandr Borzunov <hxrussia@gmail.com>
2022-07-13 01:04:48 +0000

    Change project_name to bloomd

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,5 @@ from src.bloom import *
 from src.client import *
 from src.dht_utils import declare_active_modules, get_remote_module
 
-project_name = "dibloom"
+project_name = "bloomd"
 __version__ = "0.2"
