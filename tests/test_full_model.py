84532ee5f9d5a4df808253845436620060f3ef29
justheuristic <justheuristic@gmail.com>
2022-07-22 22:21:47 +0300

    revert debug changes

--- tests/test_full_model.py
+++ tests/test_full_model.py
@@ -2,7 +2,6 @@ import pytest
 import torch
 import transformers
 from hivemind import get_logger, use_hivemind_log_handler
-
 from test_utils import *
 
 from src.client.remote_model import DistributedBloomForCausalLM
@@ -14,8 +13,9 @@ logger = get_logger(__file__)
 @pytest.mark.forked
 def test_full_model_exact_match(atol_forward=1e-3, atol_inference=1e-3):
     tokenizer = transformers.BloomTokenizerFast.from_pretrained(MODEL_NAME)
-    model = DistributedBloomForCausalLM.from_pretrained(MODEL_NAME, initial_peers=INITIAL_PEERS,
-                                                        low_cpu_mem_usage=True, torch_dtype=torch.float32)
+    model = DistributedBloomForCausalLM.from_pretrained(
+        MODEL_NAME, initial_peers=INITIAL_PEERS, low_cpu_mem_usage=True, torch_dtype=torch.float32
+    )
     assert isinstance(model, DistributedBloomForCausalLM)
     assert len(model.transformer.h) == model.config.n_layer
 
@@ -42,7 +42,8 @@ def test_full_model_exact_match(atol_forward=1e-3, atol_inference=1e-3):
 
         if REF_NAME:
             ref_model = transformers.BloomForCausalLM.from_pretrained(
-                REF_NAME, low_cpu_mem_usage=True, torch_dtype=torch.float32)
+                REF_NAME, low_cpu_mem_usage=True, torch_dtype=torch.float32
+            )
             dummy_mask = torch.ones_like(test_inputs, dtype=torch.bool)
             # note: this creates a dummy mask to make the test compatible with older transformer versions
             # prior to https://github.com/huggingface/transformers/pull/17837
