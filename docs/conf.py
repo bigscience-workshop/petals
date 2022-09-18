06a288216a25a62dd4a2d9a63a9359a4fbd12f02
justheuristic <justheuristic@gmail.com>
2022-09-18 03:15:00 +0300

    remove icon for now

--- docs/conf.py
+++ docs/conf.py
@@ -97,12 +97,12 @@ html_theme = "sphinx_rtd_theme"
 #
 html_theme_options = {"collapse_navigation": False}
 
-html_favicon = "_static/favicon.png"
+# html_favicon = "_static/favicon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
