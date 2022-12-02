85cf32d2a456398b30e26e41d1aa2bfc6982d80e
Aleksandr Borzunov <hxrussia@gmail.com>
2022-12-02 07:37:45 +0000

    Add script to run servers robustly

---
+++ src/petals/cli/run_prod_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+
+export HIVEMIND_COLORS=true
+while true; do
+        python -m petals.cli.run_server bigscience/bloom-petals \
+                --block_indices $1 \
+                --torch_dtype bfloat16 --load_in_8bit \
+                --attn_cache_size $2 2>&1 | tee log_`date '+%F_%H:%M:%S'`
+        pkill -f p2p
+        pkill -f run_server
+done
