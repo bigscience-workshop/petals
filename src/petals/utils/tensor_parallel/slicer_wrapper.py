6f65aaedb97c2e2334c7f7d7a19db452dccd7a7b
justheuristic <justheuristic@gmail.com>
2022-12-12 16:22:49 +0300

    black-isort

--- src/petals/utils/tensor_parallel/slicer_wrapper.py
+++ src/petals/utils/tensor_parallel/slicer_wrapper.py
@@ -7,173 +7,260 @@ https://github.com/BlackSamorez/petals_local_parallel/blob/496e4a8ea641ff641e593
 """
 from __future__ import annotations
 
+import dataclasses
 import re
 from copy import deepcopy
-from typing import Any, Callable, Dict, Iterator, Sequence, Tuple, Union
+from functools import partial
+from itertools import chain
+from typing import Callable, Dict, Sequence, Union
 
 import torch
-import torch.nn as nn
+from hivemind import get_logger, use_hivemind_log_handler
+from torch import nn
+from torch.nn.modules import conv
 from torch.nn.parallel import comm, replicate
 
 from petals.utils.tensor_parallel.communications import AllGather, AllReduce
 
-Pattern, Arg = str, Union[int, str]
-TensorAction = Union[str, Callable]
+Arg = Union[int, str]
+Pattern = Union[str, re.Pattern]
+Action = Union[str, Callable]  # actions describe what to do with tensors
+StateRules = Dict[Pattern, Action]  # state rules are pattern-matched actions on module state dict
+ModuleRules = Dict[Pattern, Dict[Arg, Action]]  # module rules are pattern-matched actions on inputs/outputs/attrs
+
+use_hivemind_log_handler("in_root_logger")
+logger = get_logger(__file__)
 
 
-class SlicingConfig:
-    def __init__(self, tensor_rules: Dict[Pattern, TensorAction], module_rules: Dict[Pattern, Dict[str, Any]]):
-        self.tensor_rules = tensor_rules
-        self.module_rules = module_rules
+@dataclasses.dataclass
+class Config:
+    state_rules: StateRules
+    input_rules: ModuleRules
+    output_rules: ModuleRules
+    attr_rules: ModuleRules
+
+    def __init__(
+        self, state_rules: StateRules, input_rules: ModuleRules, output_rules: ModuleRules, attr_rules: ModuleRules
+    ):
+        all_rules = [state_rules, input_rules, output_rules, attr_rules]
+        for i, rule_set in enumerate(all_rules):
+            all_rules[i] = dict((re.compile(pattern), actions) for pattern, actions in rule_set.items())
+        self.state_rules, self.input_rules, self.output_rules, self.attr_rules = all_rules
 
     @classmethod
-    def get_default_config(cls, module: nn.Module) -> SlicingConfig:
-        slicing_config = SlicingConfig({}, {})
+    def get_default_config(cls, module: nn.Module) -> Config:
+        """Make a generic config that wraps individual linear, embedding and convolutional layers"""
+        config = cls({}, {}, {}, {})
         for name, module in module.named_modules():
             if isinstance(module, nn.Linear):
-                slicing_config.tensor_rules[name + ".(weight|bias)"] = "vertical"
-                slicing_config.module_rules[name] = {"input": {}, "output": {0: "gather"}, "attributes": {}}
-        return slicing_config
+                assert module.weight.shape == (module.out_features, module.in_features)
+                assert module.bias is None or module.bias.shape == (module.out_features,)
+                config.state_rules[name + ".weight"] = "split 0"
+                config.state_rules[name + ".bias"] = "split 0"
+                config.output_rules[name] = {0: "gather -1"}
+            elif isinstance(module, (nn.Embedding, nn.EmbeddingBag)):
+                assert module.max_norm is None or module.norm_type < 2
+                assert getattr(module, "bias", None) is None or module.bias.shape == module.embedding_dim
+                config.state_rules[name + ".weight"] = "split 1"
+                config.state_rules[name + ".bias"] = "split 0"
+                config.output_rules[name] = {0: "gather -1"}
+            elif isinstance(module, conv._ConvNd) and module.groups == 1:
+                shape = [module.out_channels, module.in_channels] + list(module.kernel_size)
+                shape[:2] = shape[:2][::-1] if module.transposed else shape[:2]
+                shape = tuple(shape)
+                assert module.weight.shape == shape, f"{module.weight.shape} != {shape}"
+                assert module.bias is None or module.bias.shape == (module.out_channels,), module.bias.shape
+                config.state_rules[name + ".weight"] = "split 1" if module.transposed else "split 0"
+                config.state_rules[name + ".bias"] = "split 0"
+                config.output_rules[name] = {0: "gather 1"}
+            elif isinstance(module, conv._ConvNd) and module.groups != 1:
+                logger.warning(
+                    f"AutoConfig does not support sharding convolution layers {name} with {module.groups} groups yet; "
+                    "If you're sure that you need it, implement sharding in place of this message and open a PR"
+                )
+        return config
+
+    def create_collective_ops(self, devices):
+        """
+        Return a copy of config with thread-parallel collective operations, such as AllGather and AllReduce
+
+        :note: this function should be called during TensorParallel init, before making shards
+        """
+        return dataclasses.replace(
+            self,
+            input_rules=create_collective_ops(self.input_rules, devices),
+            output_rules=create_collective_ops(self.output_rules, devices),
+        )
+
+    def make_shard(
+        self, module: nn.Module, device: torch.device, config: Config, *, rank: int, world_size: int
+    ) -> nn.Module:
+        """Apply a tensor-parallel config to a high-level module, return module shard for a given rank and device"""
+        assert (
+            len(list(module.children())) != 0
+        ), "Please ensure module is a container (e.g. Sequential), not a single layer"
+        if device.type == "cuda":
+            (shard,) = replicate(module, (device,), detach=True)
+        else:
+            shard = deepcopy(module).to(device)
+        del module
+
+        # convert parameters and buffers
+        process_state_(shard, config, rank=rank, world_size=world_size)
+
+        # convert or wrap intermediate modules
+        unique_wrappers = {}
+        with torch.no_grad():
+            for name, submodule in shard.named_modules():
+                if submodule in unique_wrappers:
+                    continue  # wrap a module at most once
+                maybe_wrapped = config._maybe_wrap_submodule(name, submodule, rank=rank, world_size=world_size)
+                if maybe_wrapped:  # apply the same wrapper to all occurrences of the given module
+                    unique_wrappers[submodule] = maybe_wrapped
+
+        for parent in list(shard.modules()):
+            for child_name, child in list(parent.named_children()):
+                if child in unique_wrappers:
+                    setattr(parent, child_name, unique_wrappers[child])
+
+        return shard
+
+    def _maybe_wrap_submodule(self, name: str, module: nn.Module, *, rank: int, world_size: int) -> nn.Module:
+        """
+        Apply the tensor parallelism config to the specified module, return wrapped module
+
+        :note: this function does NOT apply state dict changes (state_rules), these should be applied separately!
+        :note: this function will modify the original module in-place!
+        """
+        attr_actions = find_matching_actions("attr", name, self.attr_rules)
+        input_actions = find_matching_actions("input", name, self.input_rules)
+        output_actions = find_matching_actions("output", name, self.output_rules)
+        if input_actions or output_actions:
+            process_attrs_(module, attr_actions, rank=rank, world_size=world_size)
+            module = _TensorParallelWrapper(module, input_actions, output_actions, rank=rank, world_size=world_size)
+        return module
 
 
-def slice_weight_vertical(tensor: torch.Tensor, rank: int, world_size: int) -> torch.Tensor:
-    return tensor.tensor_split(world_size, dim=-2)[rank]
+def find_matching_actions(action_type: str, name: str, rules: ModuleRules) -> Dict[Arg, Action]:
+    found_actions = {}
+    for pattern, actions_for_pattern in rules.items():
+        if pattern.search(name) is not None:
+            for key, action in actions_for_pattern.items():
+                if isinstance(key, str) and key.strip().isdigit():
+                    key = int(key)  # canonoicalize int keys
+                if found_actions.get(key, action) != action:
+                    raise ValueError(
+                        f"Found conflicting {action_type} rule for module {name}, key {key}:"
+                        f" {found_actions[key]} vs {action}"
+                    )
+                found_actions[key] = action
+    return found_actions
 
 
-def slice_bias_vertical(tensor: torch.Tensor, rank: int, world_size: int) -> torch.Tensor:
-    return tensor.tensor_split(world_size)[rank]
+def apply_action(input: torch.Tensor, action: Action, *, rank: int, world_size: int):
+    if callable(action):
+        return action(input, rank=rank)  # allreduce/allgather or a custom user-defined callable
+    action_type, *opts = action.split()
+    if action_type == "split":
+        dim = int(opts[0])
+        return torch.tensor_split(input, world_size, dim=dim)[rank]
+    if action_type == "scale":
+        return input / world_size
+    if action_type == "scale_int":
+        return input // world_size
+    raise Exception(f"unexpected action {action_type}; supported actions: split, scale, or custom user-defined")
 
 
-def slice_weight_horizontal(tensor: torch.Tensor, rank: int, world_size: int) -> torch.Tensor:
-    return tensor.tensor_split(world_size, dim=-1)[rank]
+def create_collective_ops(rules: dict, devices: Sequence[torch.device]):
+    """Initialize collective thread-parallel operations from config rules"""
+    world_size = len(devices)
+    has_cpu = any(device.type == "cpu" for device in devices)
+    unique_output_transforms = {op for output_actions in rules.values() for op in output_actions.values()}
+    transform_map = {}
+    if has_cpu:
+        reduce_op = lambda xs, destination: sum(x.to(destination) for x in xs)
+        gather_op = lambda xs, destination, dim=0: torch.cat([x.to(destination) for x in xs], dim=dim)
+    else:
+        reduce_op = comm.reduce_add  # gpu-optimized ops
+        gather_op = lambda xs, destination, dim=0: comm.gather(xs, dim=dim, destination=destination)
+
+    for transform in unique_output_transforms:
+        if callable(transform):
+            transform_map[transform] = transform  # user-defined transform, no action needed
+            continue
+
+        transform_type, *opts = transform.split()
+        if transform_type == "sum":
+            transform_map[transform] = AllReduce(world_size, reduce_op, gather_op)
+        elif transform_type == "gather":
+            dim = int(opts[0]) if opts else -1
+            transform_map[transform] = AllGather(world_size, partial(gather_op, dim=dim))
+
+    initialized_output_rules = {}
+    for pattern, output_actions in rules.items():
+        output_actions = {key: transform_map[rule] for key, rule in output_actions.items()}
+        initialized_output_rules[pattern] = output_actions
+    return initialized_output_rules
 
 
-def slice_bias_horizontal(tensor: torch.Tensor, rank: int, world_size: int) -> torch.Tensor:
-    return tensor / world_size
+@torch.no_grad()
+def process_state_(module: nn.Module, config: Config, *, rank: int, world_size: int):
+    """Modify module parameters and/or buffers in-place"""
+    # TODO monitor unused rules, warn if some rules were unused
+    for name, param in chain(module.named_parameters(), module.named_buffers()):
+        for pattern, action in config.state_rules.items():
+            if pattern.search(name) is not None:
+                param.data = apply_action(param.data, action, rank=rank, world_size=world_size).clone()
+                # note: .clone is required so that the resulting parameter owns its storage
 
 
-SLICING_RULES = {
-    ("vertical", "weight"): slice_weight_vertical,
-    ("vertical", "bias"): slice_bias_vertical,
-    ("horizontal", "weight"): slice_weight_horizontal,
-    ("horizontal", "bias"): slice_bias_horizontal,
-}
+def process_attrs_(module: nn.Module, actions: Dict[Arg, str], rank: int, world_size: int):
+    """Modify module properties in-place"""
+    assert not getattr(module, "__tensor_parallel_process_attrs", False), "process_attrs was called more than once"
+    for attr, action in actions.items():
+        setattr(module, attr, apply_action(getattr(module, attr), action, rank=rank, world_size=world_size))
+    module.__tensor_parallel_process_attrs = True
 
 
-def slice_tensors_(
-    key_parameter_iterator: Iterator[Tuple[str, nn.Parameter]], tensor_rules: Dict[Arg, str], rank: int, world_size: int
-):
-    regular_rules = [(re.compile(key), value) for key, value in tensor_rules.items()]
-
-    with torch.no_grad():
-        for name, param in key_parameter_iterator:
-            for pattern, rule in regular_rules:
-                if pattern.search(name) is not None:
-                    name_ending = name.split(".")[-1]
-                    param.data = SLICING_RULES[rule, name_ending](param.data, rank=rank, world_size=world_size).clone()
-
-
-def process_input(rules: Dict[Arg, str], rank: int, world_size: int, *args, **kwargs):
+def process_input(input_actions: Dict[Arg, str], rank: int, world_size: int, *args, **kwargs):
     extended_kwargs = dict(kwargs)
     extended_kwargs.update(enumerate(args))
-    for target, action in rules.items():
-        if not isinstance(extended_kwargs.get(target), torch.Tensor):
+    for target, action in input_actions.items():
+        if not isinstance(extended_kwargs[target], torch.Tensor):
             continue  # optional parameter is None or False
-        action_type, *maybe_dim = action
-        if action_type == "cut":
-            extended_kwargs[target] = extended_kwargs[target].tensor_split(world_size, dim=maybe_dim)[rank]
-        elif action_type == "scale":
-            extended_kwargs[target] = extended_kwargs[target] / world_size
-        else:
-            raise Exception(f"unexpected action {action_type}")
-
+        extended_kwargs[target] = apply_action(extended_kwargs[target], action, rank=rank, world_size=world_size)
     args = [extended_kwargs.pop(i) for i in range(len(args))]
     return args, extended_kwargs
 
 
-def process_output(output, rules: Dict[Arg, Callable[[torch.Tensor, int], torch.Tensor]], rank: int):
+def process_output(
+    output, output_actions: Dict[Arg, Callable[[torch.Tensor, int], torch.Tensor]], *, rank: int, world_size: int
+):
     if isinstance(output, torch.Tensor):
-        return process_output([output], rules, rank)[0]
-    for target, action in rules.items():
-        output[target] = action(output[target], rank)
+        return process_output([output], output_actions, rank=rank, world_size=world_size)[0]
+    for target, action in output_actions.items():
+        output[target] = apply_action(output[target], action, rank=rank, world_size=world_size)
     return output
 
 
-def process_attr(module: nn.Module, rules: Dict[Arg, str], rank: int, world_size: int):
-    for attr, action in rules.items():
-        if action == "scale_int":
-            setattr(module, attr, getattr(module, attr) // world_size)
-        else:
-            raise NotImplementedError(action)
+class _TensorParallelWrapper(nn.Module):
+    """Wraps a single module, applies tensor parallelism actions to module inputs and outputs"""
 
-
-class ParallelLayerWrapper(nn.Module):
-    def __init__(self, module: nn.Module, module_rules: dict, rank: int, world_size: int):
+    def __init__(
+        self,
+        module: nn.Module,
+        input_actions: Dict[Arg, Action],
+        output_actions: Dict[Arg, Action],
+        *,
+        rank: int,
+        world_size: int,
+    ):
         super().__init__()
         self.module = module
-        process_attr(self.module, module_rules["attributes"], rank=rank, world_size=world_size)
-
-        self.input_rules = module_rules["input"]
-        self.output_rules = module_rules["output"]
-
-        self.rank = rank
-        self.world_size = world_size
+        self.input_actions, self.output_actions = input_actions, output_actions
+        self.rank, self.world_size = rank, world_size
 
     def forward(self, *args, **kwargs):
-        args, kwargs = process_input(self.input_rules, self.rank, self.world_size, *args, **kwargs)
+        args, kwargs = process_input(self.input_actions, self.rank, self.world_size, *args, **kwargs)
         output = self.module(*args, **kwargs)
-        return process_output(output, self.output_rules, self.rank)
-
-
-def create_collective_ops(module_rules: dict, devices: Sequence[torch.device]):
-    world_size = len(devices)
-    if any(device.type == "cpu" for device in devices):
-        reduce_op = lambda xs, destination: sum(x.to(destination) for x in xs)
-        gather_op = lambda xs, destination: torch.cat([x.to(destination) for x in xs], dim=-1)
-    else:
-        reduce_op = comm.reduce_add  # gpu-optimized ops
-        gather_op = lambda xs, destination: comm.gather(xs, dim=-1, destination=destination)
-
-    unique_output_transforms = {op for rules in module_rules.values() for op in rules["output"].values()}
-    transform_map = {}
-    for transform in unique_output_transforms:
-        if transform == "sum":
-            transform_map[transform] = AllReduce(world_size, reduce_op, gather_op)
-        elif transform == "gather":
-            transform_map[transform] = AllGather(world_size, gather_op)
-        elif callable(transform):
-            transform_map[transform] = transform  # user-defined transform, no action needed
-        else:
-            raise NotImplementedError(f"Unknown output transform {transform}")
-
-    initialized_module_rules = {}
-    for pattern, rules in module_rules.items():
-        output_ops = {key: transform_map[rule] for key, rule in rules["output"].items()}
-        initialized_module_rules[pattern] = dict(rules, output=output_ops)
-    return initialized_module_rules
-
-
-def wrap_submodules_(model: nn.Module, module_rules: dict, rank: int, world_size: int):
-    unique_wrappers = {}
-    with torch.no_grad():
-        for name, module in model.named_modules():
-            for pattern, rule in module_rules.items():
-                if re.search(pattern, name) is not None:
-                    unique_wrappers[module] = ParallelLayerWrapper(module, rule, rank=rank, world_size=world_size)
-
-    for parent in list(model.modules()):
-        for child_name, child in list(parent.named_children()):
-            if child in unique_wrappers:
-                setattr(parent, child_name, unique_wrappers[child])
-
-
-def create_module_shard(
-    module: nn.Module, device: torch.device, config: SlicingConfig, rank: int, world_size: int
-) -> nn.Module:
-    if device.type == "cuda":
-        (replica,) = replicate(module, (device,), detach=True)
-    else:
-        replica = deepcopy(module).to(device)
-    slice_tensors_(replica.named_parameters(), config.tensor_rules, rank, world_size)
-    wrap_submodules_(nn.ModuleList([replica]), config.module_rules, rank, world_size)
-    return replica
+        return process_output(output, self.output_actions, rank=self.rank, world_size=self.world_size)
--- src/petals/utils/tensor_parallel/tensor_parallel.py
+++ src/petals/utils/tensor_parallel/tensor_parallel.py
@@ -8,7 +8,7 @@ from torch._utils import _get_all_device_indices, _get_device_index
 from torch.nn.parallel import parallel_apply
 
 from petals.utils.tensor_parallel.communications import broadcast_coalesced
-from petals.utils.tensor_parallel.slicer_wrapper import SlicingConfig, create_collective_ops, create_module_shard
+from petals.utils.tensor_parallel.slicer_wrapper import Config
 
 use_hivemind_log_handler("in_root_logger")
 logger = get_logger(__file__)
@@ -20,13 +20,11 @@ class TensorParallel(nn.Module):
         module: nn.Module,
         device_ids: Optional[Sequence[torch.device]] = None,
         output_device: Optional[torch.device] = None,
-        config: Optional[SlicingConfig] = None,
+        config: Optional[Config] = None,
     ):
-        if isinstance(module, nn.Linear):
-            # if linear is the only module, it interferes with internal slicing rules
-            module = nn.Sequential(module)
         super().__init__()
-        self.module_shards: List[nn.Module] = nn.ModuleList()
+        original_params = sum(p.numel() for p in module.parameters())
+        self.module_shards = nn.ModuleList()
         if device_ids is None:
             device_ids = _get_all_device_indices()
         if device_ids is None or len(device_ids) <= 1:
@@ -40,18 +38,19 @@ class TensorParallel(nn.Module):
         world_size = len(self.devices)
 
         if config is None:
-            config = SlicingConfig.get_default_config(module)
-            logger.info("Using automatic config: apply TP to linear layers only")
+            config = Config.get_default_config(module)
+            logger.info("Using automatic config: sharding individual linear/conv/emb layers")
 
-        module_rules_with_ops = create_collective_ops(config.module_rules, self.devices)
-        # ^-- creates collective op instances, such as AllReduce and AllGather
-        config_with_ops = SlicingConfig(config.tensor_rules, module_rules_with_ops)
+        config_with_ops = config.create_collective_ops(self.devices)
+        # ^-- creates a copy of comfig with collective op instances, such as AllReduce and AllGather
 
         for rank, device in enumerate(self.devices):
-            self.module_shards.append(create_module_shard(module, device, config_with_ops, rank, world_size))
+            self.module_shards.append(
+                config.make_shard(module, device, config_with_ops, rank=rank, world_size=world_size)
+            )
 
         # self-diagnostics: check if the model was sharded properly
-        original_params = sum(p.numel() for p in module.parameters())
+
         params_per_shard = [sum(p.numel() for p in shard.parameters()) for shard in self.module_shards]
         assert sum(params_per_shard) >= original_params, "Internal assert failed: lost some parameters during sharding"
         self.param_fractions = tuple(params_i / original_params for params_i in params_per_shard)
@@ -59,13 +58,9 @@ class TensorParallel(nn.Module):
         log_level = logging.DEBUG if inefficiency_rate < 0.1 else logging.WARNING
         logger.log(
             log_level,
-            f"Inefficiency warning: model has {original_params} but shards have {params_per_shard} params. "
+            f"Inefficiency warning: model has {original_params} params but shards have {params_per_shard} params. "
             f"This means that each GPU uses {inefficiency_rate * 100:.3f}% extra memory for parameters",
         )
-
-    def _replicate_one_input(self, inp: Union[torch.Tensor, Any]):
-        if isinstance(inp, torch.Tensor):
-            return [inp.to(device) for device in self.device_ids]
 
     def forward(self, *args, **kwargs):
         if len(self.module_shards) <= 1:
@@ -86,3 +81,12 @@ class TensorParallel(nn.Module):
             args_and_kwargs_replicated[idx] = nested_pack(args_and_kwargs_replicated[idx], args_and_kwargs)
         inputs, kwargs_tup = zip(*args_and_kwargs_replicated)
         return parallel_apply(self.module_shards, inputs, kwargs_tup=kwargs_tup)[self.output_device_index]
+
+
+class _Wrapper(nn.Module):
+    def __init__(self, module: nn.Module):
+        super().__init__()
+        self.module = module
+
+    def forward(self, *args, **kwargs):
+        return self.module(*args, **kwargs)
