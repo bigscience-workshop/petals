c4730125dfd6543e36aa73ab0918c2f98c2a60a0
justheuristic <justheuristic@gmail.com>
2022-12-13 00:42:22 +0300

    benchmark throughput in CI jobs

---
+++ tests/test_aux_functions.py
@@ -0,0 +1,13 @@
+import torch
+from test_utils import *
+
+from petals.client import DistributedBloomConfig
+from petals.server.throughput import measure_compute_rps
+
+
+def test_throughput_basic():
+    config = DistributedBloomConfig.from_pretrained(MODEL_NAME, initial_peers=INITIAL_PEERS)
+    throughput = measure_compute_rps(
+        config, device=torch.device("cpu"), dtype=torch.bfloat16, load_in_8bit=False, n_steps=10
+    )
+    assert isinstance(throughput, float) and throughput > 0
