45b87a81c95743f9372dfccd4b1f96af232e4b61
justheuristic <justheuristic@gmail.com>
2022-12-13 14:28:42 +0300

    renames

--- src/petals/utils/tensor_parallel/__init__.py
+++ src/petals/utils/tensor_parallel/__init__.py
@@ -2,8 +2,8 @@
 A prototype TensorParallel wrapper that works without torchrun. Original code by @BlackSamorez and @IaroslavLisniak .
 This code is here temporarily, with authors' permission, until they make it publicly available otherwise.
 
-The original code can be found here: https://github.com/BlackSamorez/petals_local_parallel , using MIT license
-https://github.com/BlackSamorez/petals_local_parallel/blob/496e4a8ea641ff641e59309445ddc9fe0d7960cd/LICENCE
+The original code can be found here: https://github.com/BlackSamorez/tensor_parallel , using MIT license
+https://github.com/BlackSamorez/tensor_parallel/blob/496e4a8ea641ff641e59309445ddc9fe0d7960cd/LICENCE
 """
 
 from petals.utils.tensor_parallel.slicer_wrapper import Config
