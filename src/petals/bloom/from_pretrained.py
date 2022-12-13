92e309a4fbe311cf84052ff931fb611e562b00ff
Aleksandr Borzunov <hxrussia@gmail.com>
2022-12-13 14:22:40 +0000

    Retry if downloading from HF Hub failed

--- src/petals/bloom/from_pretrained.py
+++ src/petals/bloom/from_pretrained.py
@@ -8,6 +8,8 @@ If necessary, one can rewrite this to implement a different behavior, such as:
 """
 from __future__ import annotations
 
+import itertools
+import time
 from typing import Optional, OrderedDict, Union
 
 import torch
@@ -75,12 +77,13 @@ def _load_state_dict(
     use_auth_token: Optional[str] = None,
     cache_dir: str,
     max_disk_space: Optional[int] = None,
+    min_backoff: float = 5,
 ) -> OrderedDict[str, torch.Tensor]:
     revision = BLOCK_BRANCH_PREFIX + str(block_index)
 
     # First, try to find the weights locally
-    with allow_cache_reads(cache_dir):
-        try:
+    try:
+        with allow_cache_reads(cache_dir):
             archive_file = get_file_from_repo(
                 pretrained_model_name_or_path,
                 filename=WEIGHTS_NAME,
@@ -91,25 +94,33 @@ def _load_state_dict(
             )
             if archive_file is not None:
                 return torch.load(archive_file, map_location="cpu")
-        except Exception as e:
-            logger.info(f"Failed to load block {block_index} from cache: {e}. The block will be downloaded again")
+    except Exception:
+        logger.debug(
+            f"Failed to load block {block_index} from cache. The block will be downloaded again", exc_info=True
+        )
 
     # If not found, ensure that we have enough disk space to download them (maybe remove something)
-    with allow_cache_writes(cache_dir):
-        block_size = get_block_size(config, "disk")
-        free_disk_space_for(
-            pretrained_model_name_or_path, block_size, cache_dir=cache_dir, max_disk_space=max_disk_space
-        )
+    for attempt_no in itertools.count():
+        try:
+            with allow_cache_writes(cache_dir):
+                block_size = get_block_size(config, "disk")
+                free_disk_space_for(
+                    pretrained_model_name_or_path, block_size, cache_dir=cache_dir, max_disk_space=max_disk_space
+                )
 
-        archive_file = get_file_from_repo(
-            pretrained_model_name_or_path,
-            filename=WEIGHTS_NAME,
-            revision=revision,
-            use_auth_token=use_auth_token,
-            cache_dir=cache_dir,
-            local_files_only=False,
-        )
-        return torch.load(archive_file, map_location="cpu")
+                archive_file = get_file_from_repo(
+                    pretrained_model_name_or_path,
+                    filename=WEIGHTS_NAME,
+                    revision=revision,
+                    use_auth_token=use_auth_token,
+                    cache_dir=cache_dir,
+                    local_files_only=False,
+                )
+                return torch.load(archive_file, map_location="cpu")
+        except Exception as e:
+            delay = min_backoff * (2 ** attempt_no)
+            logger.warning(f"Failed to load block {block_index} from HF Hub (retry in {delay:.0f} sec)", exc_info=True)
+            time.sleep(delay)
 
 
 DTYPE_MAP = dict(bfloat16=torch.bfloat16, float16=torch.float16, float32=torch.float32, auto="auto")
