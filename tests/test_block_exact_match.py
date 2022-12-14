0ac36b0f0bba369c563d0a50fa7b8c02f10f4c69
justheuristic <justheuristic@gmail.com>
2022-12-14 22:09:07 +0300

    slightly less invasive

--- tests/test_block_exact_match.py
+++ tests/test_block_exact_match.py
@@ -13,7 +13,7 @@ from petals.dht_utils import get_remote_module
 
 
 @pytest.mark.forked
-def test_remote_block_exact_match(atol_forward=1e-5, atol_inference=1e-3):
+def test_remote_block_exact_match(atol_forward=1e-4, atol_inference=1e-3):
     dht = hivemind.DHT(initial_peers=INITIAL_PEERS, client_mode=True, start=True)
     config = DistributedBloomConfig.from_pretrained(MODEL_NAME)
 
