10c3c636e938daa107c22aa4c4a221d3ecc2fdb4
justheuristic <justheuristic@gmail.com>
2022-12-14 19:02:38 +0300

    fix device check

--- src/petals/server/server.py
+++ src/petals/server/server.py
@@ -145,10 +145,10 @@ class Server:
                 tensor_parallel_devices = tuple(torch.device(i) for i in range(torch.cuda.device_count()))
             else:
                 tensor_parallel_devices = tuple()
-        if tensor_parallel_devices:
-            logger.info(f"Model weights will be split between {', '.join(tensor_parallel_devices)}")
-            check_device_balance(tensor_parallel_devices)
         self.tensor_parallel_devices = tuple(map(torch.device, tensor_parallel_devices))
+        if self.tensor_parallel_devices:
+            logger.info(f"Model weights will be split between {', '.join(self.tensor_parallel_devices)}")
+            check_device_balance(self.tensor_parallel_devices)
 
         assert num_blocks is None or block_indices is None, "Please specify num_blocks or block_indices, not both"
         if num_blocks is None and block_indices is None:
