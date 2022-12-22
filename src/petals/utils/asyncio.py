27663db7edfb7c69e33247a3e5a04a4788e341bf
Aleksandr Borzunov <hxrussia@gmail.com>
2022-12-22 14:55:39 +0000

    shield_and_wait(): Wrap coroutines into a task

--- src/petals/utils/asyncio.py
+++ src/petals/utils/asyncio.py
@@ -6,6 +6,9 @@ async def shield_and_wait(task):
     Works like asyncio.shield(), but waits for the task to finish before raising CancelledError to the caller.
     """
 
+    if not isinstance(task, asyncio.Task):
+        task = asyncio.create_task(task)
+
     cancel_exc = None
     while True:
         try:
