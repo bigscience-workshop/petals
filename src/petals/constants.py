712f5a330f4c9e590d9d9d06a031045c04dd611e
Alexander Borzunov <borzunov.alexander@gmail.com>
2023-01-06 10:51:12 +0400

    Remove backup bootstrap peer

--- src/petals/constants.py
+++ src/petals/constants.py
@@ -3,5 +3,4 @@ PUBLIC_INITIAL_PEERS = [
     "/dns6/bootstrap1.petals.ml/tcp/31337/p2p/QmedTaZXmULqwspJXz44SsPZyTNKxhnnFvYRajfH7MGhCY",
     "/dns/bootstrap2.petals.ml/tcp/31338/p2p/QmQGTqmM7NKjV6ggU1ZCap8zWiyKR89RViDXiqehSiCpY5",
     "/dns6/bootstrap2.petals.ml/tcp/31338/p2p/QmQGTqmM7NKjV6ggU1ZCap8zWiyKR89RViDXiqehSiCpY5",
-    "/ip4/193.106.95.184/tcp/46419/p2p/12D3KooWHqdGgDZZRCRDKqiikB1ofC3xLnV3oUynepUfDjNh5g9X",
 ]
