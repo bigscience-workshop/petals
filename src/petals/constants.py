17dcbbb152e48de51b523da0364c7b548040de6b
Aleksandr Borzunov <hxrussia@gmail.com>
2023-01-12 11:25:29 +0000

    Add comment

--- src/petals/constants.py
+++ src/petals/constants.py
@@ -5,4 +5,5 @@ PUBLIC_INITIAL_PEERS = [
     "/dns6/bootstrap2.petals.ml/tcp/31338/p2p/QmQGTqmM7NKjV6ggU1ZCap8zWiyKR89RViDXiqehSiCpY5",
 ]
 
+# The reachability API is currently used only when connecting to the public swarm
 REACHABILITY_API_URL = "http://health.petals.ml"
