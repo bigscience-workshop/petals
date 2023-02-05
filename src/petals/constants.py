baa9a0e991447c199471044f0003d6bf1554c70e
Alexander Borzunov <borzunov.alexander@gmail.com>
2023-02-06 03:33:02 +0400

    Remove excess spaces

--- src/petals/constants.py
+++ src/petals/constants.py
@@ -1,6 +1,6 @@
 PUBLIC_INITIAL_PEERS = [
     "/dns/bootstrap3.petals.ml/tcp/80/p2p/QmXbcgfPkYVbKmjPEVS8ziHvdYK7zCVPZV835Ej2ziHHq9",
-    "/dns6/bootstrap3.petals.ml/tcp/80/p2p/QmXbcgfPkYVbKmjPEVS8ziHvdYK7zCVPZV835Ej2ziHHq9",    
+    "/dns6/bootstrap3.petals.ml/tcp/80/p2p/QmXbcgfPkYVbKmjPEVS8ziHvdYK7zCVPZV835Ej2ziHHq9",
     "/dns/bootstrap2.petals.ml/tcp/31338/p2p/QmQGTqmM7NKjV6ggU1ZCap8zWiyKR89RViDXiqehSiCpY5",
     "/dns6/bootstrap2.petals.ml/tcp/31338/p2p/QmQGTqmM7NKjV6ggU1ZCap8zWiyKR89RViDXiqehSiCpY5",
     "/dns/bootstrap1.petals.ml/tcp/31337/p2p/QmedTaZXmULqwspJXz44SsPZyTNKxhnnFvYRajfH7MGhCY",
