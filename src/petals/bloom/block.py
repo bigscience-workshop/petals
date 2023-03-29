2116df08bcbdaff48d185af3554f15582d615d45
Alexander Borzunov <borzunov.alexander@gmail.com>
2023-03-29 04:21:37 +0400

    Fix deps, enable 8-bit by default for TP (#298)
    This PR fixes issues of #290:
    
    - hivemind bfloat16 codec crashed on dummy tensors (with 0 elements), see https://github.com/learning-at-home/hivemind/pull/560 (this PR makes Petals depend on the latest hivemind version from the repo, it's temporary)
    - transformers version check mismatched with the version allowed in `setup.cfg`
    
    Also:
    
    - This PR enables 8-bit by default for TP. Even though TP in 8-bit may be slower, we currently prefer to host more blocks to increase the network's stability.

--- src/petals/bloom/block.py
+++ src/petals/bloom/block.py
@@ -13,8 +13,8 @@ from transformers.models.bloom.modeling_bloom import BloomBlock, _expand_mask, _
 
 if not os.getenv("PETALS_IGNORE_DEPENDENCY_VERSION"):
     assert (
-        version.parse("4.26.0") < version.parse(transformers.__version__) < version.parse("5.0.0")
-    ), "Please install a proper transformers version: pip install transformers>=4.26.0,<5.0.0"
+        version.parse("4.25.1") <= version.parse(transformers.__version__) < version.parse("5.0.0")
+    ), "Please install a proper transformers version: pip install transformers>=4.25.1,<5.0.0"
 
 
 class WrappedBloomBlock(BloomBlock):
