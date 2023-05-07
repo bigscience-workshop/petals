0a313bf6c5d82b57103b973f6c851e5186f91cb1
Alexander Borzunov <borzunov.alexander@gmail.com>
2023-05-07 14:57:05 +0400

    Update hivemind to 1.1.8, enable efficient bfloat16 encoding (#311)
    This PR:
    
    1. Updates hivemind to 1.1.8 (includes https://github.com/learning-at-home/hivemind/pull/565)
    2. Enables efficient bfloat16 serialization by default (`USE_LEGACY_BFLOAT16 = False`)
    3. Removes logging code that was included to hivemind in https://github.com/learning-at-home/hivemind/pull/542

--- src/petals/__init__.py
+++ src/petals/__init__.py
@@ -1,6 +1,17 @@
+import os
+
+import hivemind
+
 from petals.client import *
 from petals.utils.logging import initialize_logs as _initialize_logs
 
 __version__ = "1.1.4"
 
+
+def _override_bfloat16_mode_default():
+    if os.getenv("USE_LEGACY_BFLOAT16") is None:
+        hivemind.compression.base.USE_LEGACY_BFLOAT16 = False
+
+
 _initialize_logs()
+_override_bfloat16_mode_default()
--- src/petals/utils/logging.py
+++ src/petals/utils/logging.py
@@ -4,30 +4,12 @@ import os
 from hivemind.utils import logging as hm_logging
 
 
-def in_jupyter() -> bool:
-    """Check if the code is run in Jupyter or Colab"""
-
-    try:
-        __IPYTHON__
-        return True
-    except NameError:
-        return False
-
-
 def initialize_logs():
     """Initialize Petals logging tweaks. This function is called when you import the `petals` module."""
 
     # Env var PETALS_LOGGING=False prohibits Petals do anything with logs
     if os.getenv("PETALS_LOGGING", "True").lower() in ("false", "0"):
         return
-
-    if in_jupyter():
-        os.environ["HIVEMIND_COLORS"] = "True"
-    importlib.reload(hm_logging)
-
-    # Remove log handlers from previous import of hivemind.utils.logging and extra handlers on Colab
-    hm_logging.get_logger().handlers.clear()
-    hm_logging.get_logger("hivemind").handlers.clear()
 
     hm_logging.use_hivemind_log_handler("in_root_logger")
 
