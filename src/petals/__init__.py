675bacb592bac7145d38ded2ea746da2b9b6c391
Alexander Borzunov <borzunov.alexander@gmail.com>
2023-05-10 03:01:01 +0400

    Bump version to 1.1.5 (#312)

--- src/petals/__init__.py
+++ src/petals/__init__.py
@@ -5,7 +5,7 @@ import hivemind
 from petals.client import *
 from petals.utils.logging import initialize_logs as _initialize_logs
 
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 
 
 def _override_bfloat16_mode_default():
