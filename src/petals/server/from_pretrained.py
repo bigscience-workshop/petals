d0a6a006e75c053e9020615f0563f63d535c8674
Aleksandr Borzunov <hxrussia@gmail.com>
2023-06-08 15:30:28 +0000

    Fix using free_disk_space_for()

--- src/petals/server/from_pretrained.py
+++ src/petals/server/from_pretrained.py
@@ -6,54 +6,26 @@ If necessary, one can rewrite this to implement a different behavior, such as:
  - fetch the weights over IPoAC, using a fleet of trained pigeons ( http://www.faqs.org/rfcs/rfc1149.html )
 
 """
-from __future__ import annotations
-
 import itertools
 import json
 import time
-from dataclasses import dataclass, field
-from typing import Optional, OrderedDict, Union
+from typing import Dict, Optional, Union
 
 import torch
+import torch.nn as nn
 from accelerate import init_empty_weights
 from accelerate.utils import set_module_tensor_to_device
 from hivemind.utils.logging import get_logger
+from huggingface_hub import get_hf_file_metadata, hf_hub_url
 from transformers import AutoConfig, PretrainedConfig
 from transformers.modeling_utils import WEIGHTS_NAME
-from transformers.models.bloom.modeling_bloom import BloomAttention
-from transformers.models.llama.modeling_llama import LlamaAttention
 from transformers.utils import get_file_from_repo
 
-from petals.bloom.block import WrappedBloomBlock
-from petals.llama.block import WrappedLlamaBlock
 from petals.server.block_utils import get_block_size
+from petals.server.model_specs import MODEL_SPECS
 from petals.utils.disk_cache import DEFAULT_CACHE_DIR, allow_cache_reads, allow_cache_writes, free_disk_space_for
 
 logger = get_logger(__name__)
-
-
-@dataclass(frozen=True)
-class ModelSpec:
-    block_class: Type[nn.Module]
-    attn_class: Type[nn.Module]  # An nn.Module with attention, expected to have the .num_heads attribute
-    block_prefix: str
-    config_map: Dict[str, str] = field(
-        default_factory=dict
-    )  # How to translate config keys into the bigscience/bloom format
-
-
-MODEL_SPECS = {
-    "bloom": ModelSpec(block_class=WrappedBloomBlock, attn_class=BloomAttention, block_prefix="h"),
-    "llama": ModelSpec(
-        block_class=WrappedLlamaBlock,
-        attn_class=LlamaAttention,
-        block_prefix="model.layers",
-        config_map={
-            "n_head": "num_attention_heads",
-            "n_layer": "num_hidden_layers",
-        },
-    ),
-}
 
 
 class AutoBlockConfig:
@@ -83,7 +55,7 @@ def load_pretrained_block(
     use_auth_token: Optional[str] = None,
     cache_dir: Optional[str] = None,
     max_disk_space: Optional[int] = None,
-) -> WrappedLlamaBlock:
+) -> nn.Module:
     assert torch_dtype in DTYPE_MAP.values(), f"torch_dtype must be one of {list(DTYPE_MAP.values())}"
 
     if config is None:
@@ -95,7 +67,7 @@ def load_pretrained_block(
         block = config.block_class(config)
 
     block_prefix = f"{config.block_prefix}.{block_index}."
-    state_dict = _load_state_dict(
+    state_dict = _load_state_dict_from_repo(
         model_name,
         block_prefix,
         config,
@@ -124,7 +96,10 @@ def load_pretrained_block(
     return block
 
 
-def _load_state_dict(
+StateDict = Dict[str, torch.Tensor]
+
+
+def _load_state_dict_from_repo(
     model_name: str,
     block_prefix: str,
     config: PretrainedConfig,
@@ -133,7 +108,7 @@ def _load_state_dict(
     cache_dir: str,
     max_disk_space: Optional[int] = None,
     min_backoff: float = 5,
-) -> OrderedDict[str, torch.Tensor]:
+) -> StateDict:
     index_file = get_file_from_repo(
         model_name, filename="pytorch_model.bin.index.json", use_auth_token=use_auth_token, cache_dir=cache_dir
     )
@@ -153,44 +128,49 @@ def _load_state_dict(
     # First, try to find the weights locally
     try:
         with allow_cache_reads(cache_dir):
-            archive_file = get_file_from_repo(
-                model_name,
-                filename=filename,
-                use_auth_token=use_auth_token,
-                cache_dir=cache_dir,
-                local_files_only=True,
+            return _load_state_dict_from_file(
+                model_name, filename, use_auth_token=use_auth_token, cache_dir=cache_dir, local_files_only=True
             )
-            if archive_file is not None:
-                return torch.load(archive_file, map_location="cpu")
     except Exception:
         logger.debug(
-            f"Failed to load block {block_prefix}* from cache. The block will be downloaded again", exc_info=True
+            f"Failed to load block {block_prefix}* from cache, proceeding to downloading the block", exc_info=True
         )
 
     # If not found, ensure that we have enough disk space to download them (maybe remove something)
     for attempt_no in itertools.count():
         try:
             with allow_cache_writes(cache_dir):
-                # block_size = get_block_size(config, "disk")
-                # # FIXME: Get correct file size
-                # free_disk_space_for(
-                #     model_name, block_size, cache_dir=cache_dir, max_disk_space=max_disk_space
-                # )
+                url = hf_hub_url(model_name, filename)
+                file_size = get_hf_file_metadata(url, token=use_auth_token).size
+                gib = 1024**3
+                logger.debug(f"Shard size for {filename}: {file_size / gib:.2f} GiB")
 
-                archive_file = get_file_from_repo(
-                    model_name,
-                    filename=filename,
-                    use_auth_token=use_auth_token,
-                    cache_dir=cache_dir,
-                    local_files_only=False,
+                free_disk_space_for(model_name, file_size, cache_dir=cache_dir, max_disk_space=max_disk_space)
+
+                return _load_state_dict_from_file(
+                    model_name, filename, use_auth_token=use_auth_token, cache_dir=cache_dir, local_files_only=False
                 )
-                return torch.load(archive_file, map_location="cpu")
         except Exception as e:
             delay = min_backoff * (2**attempt_no)
             logger.warning(
                 f"Failed to load block {block_prefix}* from HF Hub (retry in {delay:.0f} sec)", exc_info=True
             )
             time.sleep(delay)
+
+
+def _load_state_dict_from_file(
+    model_name: str, filename: str, *, use_auth_token: Optional[str], cache_dir: str, local_files_only: bool
+) -> StateDict:
+    path = get_file_from_repo(
+        model_name,
+        filename=filename,
+        use_auth_token=use_auth_token,
+        cache_dir=cache_dir,
+        local_files_only=local_files_only,
+    )
+    if path is None:
+        raise RuntimeError(f"Failed to load file {filename} from repo {model_name}")
+    return torch.load(path, map_location="cpu")
 
 
 DTYPE_MAP = dict(bfloat16=torch.bfloat16, float16=torch.float16, float32=torch.float32, auto="auto")
