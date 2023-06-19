7c2b0dd5e5f01a3f453441e4047e4857f6df4a37
Aleksandr Borzunov <hxrussia@gmail.com>
2023-06-19 15:21:48 +0000

    Export llama models to petals.* namespace

--- src/petals/models/llama/__init__.py
+++ src/petals/models/llama/__init__.py
@@ -1,2 +1,7 @@
 from petals.models.llama.block import WrappedLlamaBlock
 from petals.models.llama.config import DistributedLlamaConfig
+from petals.models.llama.model import (
+    DistributedLlamaForCausalLM,
+    DistributedLlamaForSequenceClassification,
+    DistributedLlamaModel,
+)
