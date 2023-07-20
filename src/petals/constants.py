54f7049695f494641cfed9dda736dce48f4056bd
Your Name <you@example.com>
2023-07-20 18:58:01 +0300

    https

--- src/petals/constants.py
+++ src/petals/constants.py
@@ -12,6 +12,6 @@ PUBLIC_INITIAL_PEERS = [
 ]
 
 # The reachability API is currently used only when connecting to the public swarm
-REACHABILITY_API_URL = "http://health.petals.dev"
+REACHABILITY_API_URL = "https://health.petals.dev"
 
 DTYPE_MAP = dict(bfloat16=torch.bfloat16, float16=torch.float16, float32=torch.float32, auto="auto")
