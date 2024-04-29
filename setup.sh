0bfd0cbd48ddce9177159b3647e39d57e9ae7b3d
mike dupont <mike.dupont@introspector.local>
2024-04-29 16:41:08 -0400

    updat

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-cp envs/gpu/dht1.cillium.dev.compute.agentartificial.com.txt  .env
-
+cp envs/dht1.cillium.dev.compute.agentartificial.com.txt  .env
 docker compose --profile miner --env-file .env up -d 
