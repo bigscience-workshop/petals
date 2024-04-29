1fcc81bccf18d298a2729211094b9cd6d53c5715
mike dupont <mike.dupont@introspector.local>
2024-04-29 16:40:04 -0400

    update

--- setup.sh
+++ setup.sh
@@ -2,18 +2,6 @@
 
 set -e
 
-if [ -z "$INITIAL_PEERS" ]; then
-    INITIAL_PEERS=cat envs/gpu/h100/peers.txt
-fi
+cp envs/gpu/dht1.cillium.dev.compute.agentartificial.com.txt  .env
 
-cat env.example > .env
-sed -i "s/INITIAL_PEERS=.*/INITIAL_PEERS=$INITIAL_PEERS/" .env
-
-python -m venv .venv
-source .venv/bin/activate
-python -m pip install --upgrade pip
-
-pip install setuptools wheel gnureadline
-pip install -e .
-
-docker compose --profile core --env-file .env up -d
+docker compose --profile miner --env-file .env up -d 
