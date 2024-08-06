2df7a328fd5fd6091d94ebd703efbd79d09ba4b1
vahe1994 <vahe527887@yandex.ru>
2024-08-06 15:13:33 +0400

    debug

--- src/petals/utils/convert_block.py
+++ src/petals/utils/convert_block.py
@@ -22,8 +22,7 @@ class QuantType(Enum):
     NF4 = 2  # 4-bit as in the QLoRA paper
 
 def is_gptq_quant(config):
-    return hasattr(config, 'quantization_config') and hasattr(config.quantization_config,
-                                                                  "quant_method") and config.quantization_config.quant_method == "gptq"
+    return hasattr(config, 'quantization_config') and ("quant_method" in config.quantization_config)
 
 def convert_block(
     block: nn.Module,
